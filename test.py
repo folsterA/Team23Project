ab7d00aefd8ca82b21c5bb4f74eb7453a8f6fed8
stuckinthemicrowave <60021754+stuckinthemicrowave@users.noreply.github.com>
2021-05-10 15:48:14 -0700

    Add files via upload
    Basic dictionary format to be used for recipe web application.

---
+++ test.py
@@ -0,0 +1,153 @@
+
+recipes = {
+  "recipeName" : "Chicken",
+  "userName" : "userName",
+  "ingredients" : [
+    {
+      "ingrediantName" : "chicken",
+      "quantity" : "1 lb"
+    },
+    {
+      "ingredientName" : "salt",
+      "quantity" : "1 tbsp"
+    },
+    {
+      "ingredientName" : "olive oil",
+      "quantity" : "2 tbsp"
+    }
+    ],
+    "tags" : ["chicken", "classic", "easy", "quick", "french", "italian"],
+    "instructions" : [
+      {
+        "1" : "1) Step 1",
+        "2" : "2) Step 2",
+        "3" : "3) Step 3"
+      }
+    ],
+    "urlFor" : "www.chicken.org"
+},
+{
+  "recipeName" : "Steak",
+  "userName" : "userName",
+  "ingredients" : [
+    {
+      "ingredientName" : "beef",
+      "quantity" : "1 lb"
+    },
+    {
+      "ingredientName" :"salt", 
+      "quantity" : "1 tbsp"
+    },
+    {
+      "ingredientName" : "pepper",
+      "quantity" : "1/2 tbsp"
+    },
+    {
+      "ingredientName" : "canola oil",
+      "quantity" : "2tbsp"
+    }
+    ],
+    "tags" : ["French", "American", "classic", "barbeque", "quick", "easy"],
+    "instructions" : [
+      {
+        "1" : "1) Step 1",
+        "2" : "2) Step 2",
+        "3" : "3) Step 3"
+      }
+
+    ],
+    "urlFor" : "www.beef.org"
+},
+{
+  "recipeName" : "Rice", 
+  "userName" : "userName",
+  "ingredients" : [
+    {
+      "ingredientName" : "rice",
+      "quantity" : "1 cup"
+    },
+    {
+      "ingredientName" : "water",
+      "quantity" : "1.25 cups"
+    },
+    {
+      "ingredientName" : "salt",
+      "quantity" : "a pinch"
+    },
+    {
+      "ingredientName" : "rice vinegar",
+      "quantity" : "1 tbsp"
+    }
+    ],
+    "tags" : ["asian", "simple", "quick"],
+    "instructions" : [
+      {
+        "1" : "1) Step 1",
+        "2" : "2) Step 2",
+        "3" : "3) Step 3"
+      }
+
+    ],
+    "urlFor" : "www.rice.org"
+},
+{
+  "recipeName" : "French Onion Soup", 
+  "userName" : "Lori",
+  "ingredients" : [
+    {
+      "ingredientName" : "Unsalted Butter",
+      "quantity" : "1/2 Cup"
+    },
+    {
+      "ingredientName" : "Olive Oil",
+      "quantity" : "2 tbsp"
+    },
+    {
+      "ingredientName" : "Sliced Onions",
+      "quantity" : "4 cups"
+    },
+    {
+        "ingredientName" : "Beef Broth",
+        "quantity" : "4(10.5 ounce) cans"
+    },
+    {
+        "ingredientName" : "Dry Sherry",
+        "quantity" : "2 tbsp"
+    },
+    {
+        "ingredientName" : "Dried Thyme",
+        "quantity" : "1 tbsp"
+    },
+    {
+        "ingredientName" : "Salt and Pepper",
+        "quantity" : "to taste"
+    },
+    {
+        "ingredientName" : "French Bread",
+        "quantity" : "4 slices"
+    },
+    {
+        "ingredientName" : "Provolone cheese",
+        "quantity" : "4 slices"
+    },
+    {
+        "ingredientName" : "Swiss cheese (diced)",
+        "quantity" : "2 slices"
+    },
+    {
+        "ingredientName" : "Parmesan cheese (grated)",
+        "quantity" : "1/4 cup"
+    }
+    ],
+    "tags" : ["French", "soup", "simple", "quick dinner", "homey", "comfort", "comfort food"],
+    "instructions" : [
+      {
+        "1" : "3) Melt butter with olive oil in an 8 quart stock pot on medium heat. Add onions and continually stir until tender and translucent. Do not brown the onions.",
+        "2" : "2) Add beef broth, sherry and thyme. Season with salt and pepper, and simmer for 30 minutes.",
+        "3" : "3) Heat the oven broiler",
+        "4" : "4) Ladle soup into oven safe serving bowls and place one slice of bread on top of each (bread may be broken into pieces if you prefer). Layer each slice of bread with a slice of provolone, 1/2 slice diced Swiss and 1 tablespoon Parmesan cheese. Place bowls on cookie sheet and broil in the preheated oven until cheese bubbles and browns slightly."
+      }
+
+    ],
+    "urlFor" : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F2094173.jpg&w=596&h=399&c=sc&poi=face&q=85"
+}
