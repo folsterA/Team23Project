e5d9f5437f6f74269f3c07bd0ef8ee6711e87a1c
stuckinthemicrowave <60021754+stuckinthemicrowave@users.noreply.github.com>
2021-05-17 20:58:49 -0700

    Add files via upload
    Just got back to my main driver, let me know if anything can be made prettier.

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 
-recipes = {
+recipes = [
+    {
   "recipeName" : "Chicken",
   "userName" : "userName",
   "ingredients" : [
@@ -150,4 +151,426 @@ recipes = {
 
     ],
     "urlFor" : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F2094173.jpg&w=596&h=399&c=sc&poi=face&q=85"
+},
+{
+  "recipeName" : "Juicy SteakHouse Burgers",
+  "userName" : "Jennifer Segal",
+  "ingredients" : [
+    {
+      "ingrediantName" : "white sandwich bread, crusts removed, 1/4 inch pieces",
+      "quantity" : "2 pieces"
+    },
+    {
+      "ingredientName" : "milk",
+      "quantity" : "1/3 cup"
+    },
+    {
+      "ingredientName" : "salt",
+      "quantity" : "2-1/2 teaspoons"
+    },
+    {
+        "ingredientName" : "fresh ground black pepper",
+        "quantity" : "1 teaspoon"
+    },
+    {
+        "ingredientName" : "garlic cloves, minced",
+        "quantity" : "1 teaspoon"
+    },
+    {
+        "ingredientName" : "Worcestershire sauce",
+        "quantity" : "1-1/2 tablesppons"
+    },
+    {
+        "ingredientName" : "ketchup",
+        "quantity" : "2 tablespoons"
+    },
+    {
+        "ingredientName" : "lean ground beef",
+        "quantity" : "3 lbs"
+    },
+    {
+        "ingredientName" : "scallions, very fine sliced",
+        "quantity" : "3"
+    },
+    {
+        "ingredientName" : "Hamburger Buns",
+        "quantity" : "8"
+    }
+    ],
+    "tags" : ["Burgers", "classic", "easy", "quick", "American", "simple"],
+    "instructions" : [
+      {
+        "1" : "1)  Preheat the grill to high heat.",
+        "2" : "2)  In a large bowl, mash the bread and milk together with a fork until it forms a chunky paste. Add the salt, pepper, garlic, Worcestershire sauce and ketchup and mix well.",
+        "3" : "3)  Add the ground beef and scallions and break the meat up with your hands. Mix everything together until just combined. Divide the mixture into 8 equal portions and form balls. Flatten the balls into 3/4-inch patties about 4-1/2 inches across. Form a slight depression in the center of each patty to prevent the burgers from puffing up on the grill.",
+        "4" : "4) Oil the grilling grates. Grill the burgers, covered, until nicely browned on the first side, 2 to 4 minutes. Flip burgers and continue cooking for a few minutes more until desired doneness is reached. Before serving, toast the buns on the cooler side of the grill if desired."
+      }
+    ],
+    "urlFor" : "https://www.onceuponachef.com/images/2012/04/burgers-on-grill.jpg"
+},
+{
+  "recipeName" : "Steak",
+  "userName" : "userName",
+  "ingredients" : [
+    {
+      "ingredientName" : "beef",
+      "quantity" : "1 lb"
+    },
+    {
+      "ingredientName" :"salt", 
+      "quantity" : "1 tbsp"
+    },
+    {
+      "ingredientName" : "pepper",
+      "quantity" : "1/2 tbsp"
+    },
+    {
+      "ingredientName" : "canola oil",
+      "quantity" : "2tbsp"
+    }
+    ],
+    "tags" : ["French", "American", "classic", "barbeque", "quick", "easy"],
+    "instructions" : [
+      {
+        "1" : "1) Step 1",
+        "2" : "2) Step 2",
+        "3" : "3) Step 3"
+      }
+
+    ],
+    "urlFor" : "www.beef.org"
+},
+{
+  "recipeName" : "Chicken Parmesan", 
+  "userName" : "Chef John",
+  "ingredients" : [
+    {
+      "ingredientName" : "Skinless, boneless chicken breast halves",
+      "quantity" : "4"
+    },
+    {
+      "ingredientName" : "salt and pepper",
+      "quantity" : "to taste"
+    },
+    {
+      "ingredientName" : "eggs",
+      "quantity" : "2"
+    },
+    {
+      "ingredientName" : "panko bread crumbs",
+      "quantity" : "1 cup"
+    },
+    {
+        "ingredientName" : "grated Parmesan Cheese",
+        "quantity" : "1/2 cup"
+    },
+    {
+        "ingredientName" : "all-purpose flour",
+        "quantity" : "2 tablespoons"
+    },
+    {
+        "ingredientName" : "olive oil",
+        "quantity" : "1 cup"
+    },
+    {
+        "ingredientName" : "tomato sauce",
+        "quantity" : "1/2 cup"
+    },
+    {
+        "ingredientName" : "mozzarella, cut into small cubes",
+        "quantity" : "1/4 cup"
+    },
+    {
+        "ingredientName" : "Basil, chopped, fresh",
+        "quantity" : "1/4 cup"
+    },
+    {
+        "ingredientName" : "Provolone cheese",
+        "quantity" : "1/4 cup grated"
+    },
+    {
+        "ingredientName" : "Parmesan cheese",
+        "quantity" : " 1/4 cup"
+    },
+    {
+        "ingredientName" : "Olive oil",
+        "quantity" : "1 tablespoon"
+    }
+    ],
+    "tags" : ["Italian", "simple", "easy", "harder than it looks", "fried", "classic"],
+    "instructions" : [
+      {
+        "1" : "1) Preheat an oven to 450 degrees F (230 degrees C).",
+        "2" : "2) Place chicken breasts between two sheets of heavy plastic (resealable freezer bags work well) on a solid, level surface. Firmly pound chicken with the smooth side of a meat mallet to a thickness of 1/2-inch. Season chicken thoroughly with salt and pepper.",
+        "3" : "3) Beat eggs in a shallow bowl and set aside.",
+        "4" : "4) Mix bread crumbs and 1/2 cup Parmesan cheese in a separate bowl, set aside.",
+        "5" : "5) Place flour in a sifter or strainer; sprinkle over chicken breasts, evenly coating both sides.",
+        "6" : "6) Dip flour coated chicken breast in beaten eggs. Transfer breast to breadcrumb mixture, pressing the crumbs into both sides. Repeat for each breast. Set aside breaded chicken breasts for about 15 minutes.",
+        "7" : "7) Heat 1 cup olive oil in a large skillet on medium-high heat until it begins to shimmer. Cook chicken until golden, about 2 minutes on each side. The chicken will finish cooking in the oven.",
+        "8" : "8) Place chicken in a baking dish and top each breast with about 1/3 cup of tomato sauce. Layer each chicken breast with equal amounts of mozzarella cheese, fresh basil, and provolone cheese. Sprinkle 1 to 2 tablespoons of Parmesan cheese on top and drizzle with 1 tablespoon olive oil.",
+        "9" : "9) Bake in the preheated oven until cheese is browned and bubbly, and chicken breasts are no longer pink in the center, 15 to 20 minutes. An instant-read thermometer inserted into the center should read at least 165 degrees F (74 degrees C).",
+      }
+
+    ],
+    "urlFor" : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F3371942.jpg&w=768&h=768&c=sc&poi=face&q=85"
+},
+{
+  "recipeName" : "Good Old Fashioned Pancakes",
+  "userName" : "Dakota Kelly",
+  "ingredients" : [
+    {
+      "ingredientName" : "All Purpose Flour",
+      "quantity" : "1-1/2 cups"
+    },
+    {
+      "ingredientName" :"Baking Powder", 
+      "quantity" : "3-1/2 teaspoons"
+    },
+    {
+      "ingredientName" : "salt",
+      "quantity" : "1 teaspoon"
+    },
+    {
+      "ingredientName" : "white sugar",
+      "quantity" : "1 tablespoon"
+    },
+    {
+      "ingredientName" : "milk",
+      "quantity" : "1-1/4 cups"
+    },
+    {
+      "ingredientName" : "eggs",
+      "quantity" : "1"
+    },
+    {
+      "ingredientName" : "butter, melted",
+      "quantity" : "3 tablespoons"
+    }
+    ],
+    "tags" : ["classic", "American", "homemade", "skillet", "quick", "easy"],
+    "instructions" : [
+      {
+        "1" : "1) In a large bowl, sift together the flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.",
+        "2" : "2)  Heat a lightly oiled griddle or frying pan over medium-high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot.",
+        "3" : "3) Enjoy that sh**t, honestly, a well made pancake is better than most anything, save for a kiss from a woman you love. Appreciate it."
+      }
+
+    ],
+    "urlFor" : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F3138358.jpg&w=303&h=303&c=sc&poi=face&q=85"
+},
+{
+  "recipeName" : "Scrambled Eggs",
+  "userName" : "Tori",
+  "ingredients" : [
+    {
+      "ingredientName" : "Eggs",
+      "quantity" : "4 large"
+    },
+    {
+      "ingredientName" :"Milk", 
+      "quantity" : "1 tbsp"
+    },
+    {
+      "ingredientName" : "crumbled or shredded cheese",
+      "quantity" : "to taste"
+    },
+    {
+      "ingredientName" : "salt & pepper",
+      "quantity" : "to taste"
+    },
+    {
+      "ingredientName" : "olive oil",
+      "quantity" : "1 tbsp or spray"
+    }
+    ],
+    "tags" : ["French", "American", "classic", "breakfast", "quick", "easy", "classic"],
+    "instructions" : [
+      {
+        "1" : "1) If you plan on adding cheese to your eggs, make sure that the cheese is shredded/prepared and set aside so you can add it to the skillet quickly. Place your skillet on the stovetop and turn on the heat. I leave the heat somewhere between medium low and medium, so it warms up but doesn't get too hot.",
+        "2" : "2) Meanwhile, prepare your eggs. Break them into a bowl with a tablespoon of water or milk, some salt and pepper. I use about 1/8 tsp salt and a pinch of pepper; you might use more or less according to taste. If you're dairy intolerant or don't like the taste of milk you can use water. I personally love the way milk enhances the flavor of the eggs.",
+        "3" : "3) Use a fork or whisk to beat the eggs briskly for 30-60 seconds, making sure the eggs are fully broken up and mixed well with the milk and the seasonings. Use a little elbow grease here, the more you whip it the better.",
+        "4" : "4) Lightly grease your hot skillet, coating the surface with a thin layer of oil or butter (or a combination of oil and butter). If using spray oil, use caution and keep it away from any open gas flames.",
+        "5" : "5) Pour the eggs into the skillet. Keep the heat on medium/medium low, you don't want to rush it here-- if the skillet is too hot the eggs will cook too quickly and become rubbery. Once you pour the eggs in they will begin to cook immediately. Using a spatula (I use a wooden or silicone spatula so I won't damage my pan's nonstick coating), begin pulling the cooked outer edges in towards the center of the eggs. Uncooked eggs will flood the area you just pulled back. If you are adding cheese, now is the time to sprinkle it into the skillet. This will allow ample time for the cheese to melt and integrate into the eggs.",
+        "6" : "6) Move the spatula around the edge of the skillet, pulling the cooked edges towards the center and re-flooding repeatedly. Cooked scrambled eggs will gather in the center of the skillet.",
+        "7" : "7) At a certain point, the uncooked eggs will no longer flood and the scramble will all collect in the center of the skillet, but it will still be slightly runny in texture. Begin breaking up the scramble; quickly turn undercooked areas and keep the scramble moving to make sure that all surfaces cook evenly. Never leave a surface in contact too long with the skillet or it will become overcooked.",
+        "8" : "8) Turn off the heat when the eggs are 90% cooked. When the eggs are done, serve immediately. Perfectly cooked scrambled eggs are moist but not runny, with no crisp or brown edges. This technique may take a bit of practice, but it is quite simple. With time you too will be making and serving moist, fluffy scrambled eggs!"
+      }
+
+    ],
+    "urlFor" : "https://toriavey.com/images/2014/06/scrambled_eggs_002-1.jpg"
+},
+{
+  "recipeName" : "French Toast",
+  "userName" : "Jan Bittner",
+  "ingredients" : [
+    {
+      "ingredientName" : "Thick slices, bread",
+      "quantity" : "6"
+    },
+    {
+      "ingredientName" :"eggs", 
+      "quantity" : "2"
+    },
+    {
+      "ingredientName" : "milk",
+      "quantity" : "2/3 cups"
+    },
+    {
+      "ingredientName" : "cinnamon",
+      "quantity" : "1/4 teaspoon"
+    },
+    {
+      "ingredientName" : "nutmeg",
+      "quantity" : "1/4 teaspoon"
+    },
+    {
+      "ingredientName" : "vanilla extract",
+      "quantity" : "1 teaspoon"
+    },
+    {
+      "ingredientName" : "salt",
+      "quantity" : "to taste"
+    }
+    ],
+    "tags" : ["French", "American", "classic", "breakfast", "quick", "easy", "goddamn delicious", "toe-lickin' good", "painfully delightful", "essentially, a replacement for physical contact"],
+    "instructions" : [
+      {
+        "1" : "1)  Beat together egg, milk, salt, desired spices and vanilla.",
+        "2" : "2) Heat a lightly oiled griddle or skillet over medium-high heat.",
+        "3" : "3) Dunk each slice of bread in egg mixture, soaking both sides. Place in pan, and cook on both sides until golden. Serve hot."
+      }
+
+    ],
+    "urlFor" : "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F5856757.jpg&w=766&h=766&c=sc&poi=face&q=85"
+},
+{
+  "recipeName" : "Coffee",
+  "userName" : "Luke (gotHisShitTogether) Avra",
+  "ingredients" : [
+    {
+      "ingredientName" : "coffee",
+      "quantity" : "few grams, I dunno, eyeball it"
+    },
+    {
+      "ingredientName" :"heated water", 
+      "quantity" : "with that whole eyeball thing, just do like an ounce per gram, you know?"
+    },
+    {
+      "ingredientName" : "whiskey",
+      "quantity" : "How much water you use? Match it."
+    },
+    {
+      "ingredientName" : "Literally any other booze",
+      "quantity" : "How do you want your day to look?"
+    }
+    ],
+    "tags" : ["French", "American", "classic", "German", "quick", "easy", "Hemingway-esque", "#noplans", "bottle up, bottle down"],
+    "instructions" : [
+      {
+        "1" : "1) Depending on the steeping method, place the coffee (after grinding it) in whichever container you plan to use and find a way for the water to be heated and poured over the grinds.",
+        "2" : "2) Apply booze.",
+        "3" : "3) This step kind of depends on how the 'coffee' was prepared, but this is where you make "
+      }
+
+    ],
+    "urlFor" : "www.beef.org"
+},
+{
+  "recipeName" : "Noodles", 
+  "userName" : "Luke",
+  "ingredients" : [
+    {
+      "ingredientName" : "Noodles, I don't care what type but don't listen to the portion control unless it's flat pasta (spaghetti, angel hair, you know what I mean)",
+      "quantity" : "Able to hold it in an 'okay sign?, that's enough for like 4 people. 3 if they're overweight or you've got big-ass hands. "
+    },
+    {
+      "ingredientName" : "water",
+      "quantity" : "Way more than you think, it'll all be drained and fucking salt it goddamn it. Don't be a heathen. Ya gotta season while you cook not just at the end."
+    },
+    {
+      "ingredientName" : "pepper",
+      "quantity" : "to taste and, I'm not fucking joking. ONLY. AFTER. EVERYTHING. IS. FINISHED. COOKING."
+    },
+    {
+      "ingredientName" : "rice vinegar",
+      "quantity" : "1 tbsp"
+    }
+    ],
+    "tags" : ["asian", "simple", "quick"],
+    "instructions" : [
+      {
+        "1" : "1) Step 1",
+        "2" : "2) Step 2",
+        "3" : "3) Step 3"
+      }
+
+    ],
+    "urlFor" : "www.rice.org"
+},
+{
+  "recipeName" : "Steak",
+  "userName" : "userName",
+  "ingredients" : [
+    {
+      "ingredientName" : "beef",
+      "quantity" : "1 lb"
+    },
+    {
+      "ingredientName" :"salt", 
+      "quantity" : "1 tbsp"
+    },
+    {
+      "ingredientName" : "pepper",
+      "quantity" : "1/2 tbsp"
+    },
+    {
+      "ingredientName" : "canola oil",
+      "quantity" : "2tbsp"
+    }
+    ],
+    "tags" : ["French", "American", "classic", "barbeque", "quick", "easy"],
+    "instructions" : [
+      {
+        "1" : "1) Step 1",
+        "2" : "2) Step 2",
+        "3" : "3) Step 3"
+      }
+
+    ],
+    "urlFor" : "www.beef.org"
+},
+{
+  "recipeName" : "Toad in the Hole", 
+  "userName" : "Luke Goddamn Avra",
+  "ingredients" : [
+    {
+      "ingredientName" : "Whatever bread you enjoy",
+      "quantity" : "however many you want. It's one slice per"
+    },
+    {
+      "ingredientName" : "eggs",
+      "quantity" : "1 egg per slice, good old kiss, keep it simple stupid"
+    },
+    {
+      "ingredientName" : "oil, whatever you got, you need fat, the more cholesterol the better she tastes(butter beats all)",
+      "quantity" : "I use more than most but you can probably get by with anything less than 1/2 lb"
+    },
+    {
+      "ingredientName" : "salt, pepper, maybe some sriracha",
+      "quantity" : "to taste, just flavour it how you'd like, don't be a pussy and listen to what a recipe tells you"
+    }
+    ],
+    "tags" : ["what Luke likes", "simple", "quick", "shit no-one wants to actually eat", "things that helped Luke survive the early 2000's", "It's really not that terrible, I swear, give it a try"],
+    "instructions" : [
+      {
+        "1" : "1) Whip those eggs up, don't be a lil' bitch, give 'em a good 'ol beating (just egg, no milk, no water we're purebred here)",
+        "2" : "2) Use a cup and drill a hole in one of those pieces of bread you love oh so much then take a breathe and take a lil' dip in that mixture you just created. (Just drown that little fucker in egg. I swear it works out.)",
+        "3" : "3) Toss that bad boy on a smoking hot skillet and let him cook till he's a nice golden brown, then flip till you've got the same. I'm a big proponent of cast-iron but a nice all-clad stainless steel works great."
+      }
+
+    ],
+    "urlFor" : "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/11/2/2/FNK_Hangover-Toad-in-Hole-Sandwich_s4x3.jpg.rend.hgtvcom.616.462.suffix/1447284804948.jpeg"
 }
+
+
+]
