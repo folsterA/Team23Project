1bd1f676eeb4b01c7cb6fe61d57b60a88e0ebad4
Austin Folster <folsteraustin@gmail.com>
2021-05-19 16:59:26 -0700

    Add files via upload, updated project main

---
+++ README.txt
@@ -0,0 +1,2 @@
+https://trello.com/b/qvEiZ6M4/team23-project
+https://github.com/folsterA/Team23Project
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ email: folstera@csumb.edu
 desc: This is the main python script for running the website and handling redirects 
 and templates
 '''
-
+import json
 from flask import Flask, render_template, flash, redirect
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
@@ -16,32 +16,33 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'team23'
 bootstrap = Bootstrap(app)
 
+recipes = Recipes
+
 class Recipe_Search(FlaskForm):
     recipe_keyword = StringField('Search Recipes:', validators = [DataRequired()])
 
 class Recipe_Edit(FlaskForm):
     name = StringField("Recipe Name: ", validators = [DataRequired()])
     user_name = StringField("Submitted by User: ", validators = [DataRequired()])
-    ingredients = StringField("Ingredient??: ", validators = [DataRequired()])
+    ingredients = StringField("Ingredient: ", validators = [DataRequired()])
     ethnicity = StringField("Where it's from: ", validators = [DataRequired()])
     tags = StringField("Tags (separate by commas): ", validators = [DataRequired()])
     instructions = StringField("Instructions: ", validators = [DataRequired()])
     imageURL = StringField("Instructions: ", validators = [DataRequired()])
 
-
 @app.route('/', methods=('GET', 'POST')) # index
 def index():
+    print(recipes)
     form = Recipe_Search()
     if form.validate_on_submit():
         keyword = form.recipe_keyword.data
         return redirect(f'/search/{keyword}')
     return render_template('index.html', form = form)
 
-
 @app.route('/search/<keyword>') # search
 def search(keyword):
     list = []
-    for recipe in Recipes:
+    for recipe in recipes:
         for term in recipe["recipeName"].split():  
             for key in keyword.split():
                 if key.lower() == term.rstrip(',').lower():   
@@ -55,15 +56,22 @@ def search(keyword):
     
     return render_template('search.html', keyword = keyword, data = list)
 
-@app.route('/submit') #submit
+@app.route('/submit', methods=('GET', 'POST')) #submit
 def submit():
     form = Recipe_Edit()
     if form.validate_on_submit():
-        pass
-    return render_template('submit.html')
+        recipe = {}
+        recipe["recipeName"].append(form.name)
+        recipe["userName"].append(form.user_name)
+        recipe["ingredients"].append(form.ingredients.split(", "))
+        recipe["tags"].append(form.tags.split(", "))
+        recipe["instructions"].append(form.instructions)
+        recipe["urlFor"].append(form.imageURL)
+        recipes.append(recipe)
+    return render_template('submit.html', form=form)
 
 @app.route('/recipe/<name>')
 def recipe(name):
-    for recipe in Recipes:
+    for recipe in recipes:
         if (recipe["recipeName"] == name):
             return render_template("recipe.html", name = name, data = recipe)
